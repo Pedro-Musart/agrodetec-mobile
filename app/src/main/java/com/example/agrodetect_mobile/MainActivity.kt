4fe0e55860a6203edd279a50b8985290334e950a
Pedro Soares <pedrowesleyfs@gmail.com>
2023-06-08 02:23:15 -0300

    add video and home

--- app/src/main/java/com/example/agrodetect_mobile/MainActivity.kt
+++ app/src/main/java/com/example/agrodetect_mobile/MainActivity.kt
@@ -8,14 +8,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.MediaController
 import android.widget.Toast
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         val btnStartNow = findViewById<Button>(R.id.btnStartNow)
+        val videoView = findViewById<VideoView>(R.id.video_view)
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        val offlineUri = Uri.parse("android.resource://$packageName/${R.raw.video}")
+
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(offlineUri)
+        videoView.requestFocus()
+        videoView.start()
+
+
 
         btnStartNow.setOnClickListener{
             val i = Intent(this,  LoginActivity::class.java)
